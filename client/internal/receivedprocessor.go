cb960658a40715fdc3fc52f58e049539936a6eab
Tigran Najaryan <tnajaryan@splunk.com>
2023-06-16 14:18:04 -0400

    Add OpAMP connection settings capability

--- client/internal/receivedprocessor.go
+++ client/internal/receivedprocessor.go
@@ -13,7 +13,7 @@ type receivedProcessor struct {
 	logger types.Logger
 
 	// Callbacks to call for corresponding messages.
-	callbacks types.Callbacks
+	callbacks *CallbacksWrapper
 
 	// A sender to cooperate with when the received message has an impact on
 	// what will be sent later.
@@ -30,7 +30,7 @@ type receivedProcessor struct {
 
 func newReceivedProcessor(
 	logger types.Logger,
-	callbacks types.Callbacks,
+	callbacks *CallbacksWrapper,
 	sender Sender,
 	clientSyncedState *ClientSyncedState,
 	packagesStateProvider types.PackagesStateProvider,
@@ -50,96 +50,93 @@ func newReceivedProcessor(
 // the received message and performs any processing necessary based on what fields are set.
 // This function will call any relevant callbacks.
 func (r *receivedProcessor) ProcessReceivedMessage(ctx context.Context, msg *protobufs.ServerToAgent) {
-	if r.callbacks != nil {
-		if msg.Command != nil {
-			r.rcvCommand(msg.Command)
-			// If a command message exists, other messages will be ignored
-			return
-		}
+	if msg.Command != nil {
+		r.rcvCommand(msg.Command)
+		// If a command message exists, other messages will be ignored
+		return
+	}
 
-		scheduled, err := r.rcvFlags(ctx, protobufs.ServerToAgentFlags(msg.Flags))
-		if err != nil {
-			r.logger.Errorf("cannot processed received flags:%v", err)
-		}
+	scheduled, err := r.rcvFlags(ctx, protobufs.ServerToAgentFlags(msg.Flags))
+	if err != nil {
+		r.logger.Errorf("cannot processed received flags:%v", err)
+	}
 
-		msgData := &types.MessageData{}
+	msgData := &types.MessageData{}
 
-		if msg.RemoteConfig != nil {
-			if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_AcceptsRemoteConfig) {
-				msgData.RemoteConfig = msg.RemoteConfig
-			} else {
-				r.logger.Debugf("Ignoring RemoteConfig, agent does not have AcceptsRemoteConfig capability")
-			}
-		}
-
-		if msg.ConnectionSettings != nil {
-			if msg.ConnectionSettings.OwnMetrics != nil {
-				if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_ReportsOwnMetrics) {
-					msgData.OwnMetricsConnSettings = msg.ConnectionSettings.OwnMetrics
-				} else {
-					r.logger.Debugf("Ignoring OwnMetrics, agent does not have ReportsOwnMetrics capability")
-				}
-			}
-
-			if msg.ConnectionSettings.OwnTraces != nil {
-				if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_ReportsOwnTraces) {
-					msgData.OwnTracesConnSettings = msg.ConnectionSettings.OwnTraces
-				} else {
-					r.logger.Debugf("Ignoring OwnTraces, agent does not have ReportsOwnTraces capability")
-				}
-			}
-
-			if msg.ConnectionSettings.OwnLogs != nil {
-				if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_ReportsOwnLogs) {
-					msgData.OwnLogsConnSettings = msg.ConnectionSettings.OwnLogs
-				} else {
-					r.logger.Debugf("Ignoring OwnLogs, agent does not have ReportsOwnLogs capability")
-				}
-			}
-
-			if msg.ConnectionSettings.OtherConnections != nil {
-				if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_AcceptsOtherConnectionSettings) {
-					msgData.OtherConnSettings = msg.ConnectionSettings.OtherConnections
-				} else {
-					r.logger.Debugf("Ignoring OtherConnections, agent does not have AcceptsOtherConnectionSettings capability")
-				}
-			}
-		}
-
-		if msg.PackagesAvailable != nil {
-			if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_AcceptsPackages) {
-				msgData.PackagesAvailable = msg.PackagesAvailable
-				msgData.PackageSyncer = NewPackagesSyncer(
-					r.logger,
-					msgData.PackagesAvailable,
-					r.sender,
-					r.clientSyncedState,
-					r.packagesStateProvider,
-				)
-			} else {
-				r.logger.Debugf("Ignoring PackagesAvailable, agent does not have AcceptsPackages capability")
-			}
-		}
-
-		if msg.AgentIdentification != nil {
-			err := r.rcvAgentIdentification(msg.AgentIdentification)
-			if err == nil {
-				msgData.AgentIdentification = msg.AgentIdentification
-			}
-		}
-
-		r.callbacks.OnMessage(ctx, msgData)
-
-		r.rcvOpampConnectionSettings(ctx, msg.ConnectionSettings)
-
-		if scheduled {
-			r.sender.ScheduleSend()
+	if msg.RemoteConfig != nil {
+		if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_AcceptsRemoteConfig) {
+			msgData.RemoteConfig = msg.RemoteConfig
+		} else {
+			r.logger.Debugf("Ignoring RemoteConfig, agent does not have AcceptsRemoteConfig capability")
 		}
 	}
 
-	err := msg.GetErrorResponse()
-	if err != nil {
-		r.processErrorResponse(err)
+	if msg.ConnectionSettings != nil {
+		if msg.ConnectionSettings.OwnMetrics != nil {
+			if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_ReportsOwnMetrics) {
+				msgData.OwnMetricsConnSettings = msg.ConnectionSettings.OwnMetrics
+			} else {
+				r.logger.Debugf("Ignoring OwnMetrics, agent does not have ReportsOwnMetrics capability")
+			}
+		}
+
+		if msg.ConnectionSettings.OwnTraces != nil {
+			if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_ReportsOwnTraces) {
+				msgData.OwnTracesConnSettings = msg.ConnectionSettings.OwnTraces
+			} else {
+				r.logger.Debugf("Ignoring OwnTraces, agent does not have ReportsOwnTraces capability")
+			}
+		}
+
+		if msg.ConnectionSettings.OwnLogs != nil {
+			if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_ReportsOwnLogs) {
+				msgData.OwnLogsConnSettings = msg.ConnectionSettings.OwnLogs
+			} else {
+				r.logger.Debugf("Ignoring OwnLogs, agent does not have ReportsOwnLogs capability")
+			}
+		}
+
+		if msg.ConnectionSettings.OtherConnections != nil {
+			if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_AcceptsOtherConnectionSettings) {
+				msgData.OtherConnSettings = msg.ConnectionSettings.OtherConnections
+			} else {
+				r.logger.Debugf("Ignoring OtherConnections, agent does not have AcceptsOtherConnectionSettings capability")
+			}
+		}
+	}
+
+	if msg.PackagesAvailable != nil {
+		if r.hasCapability(protobufs.AgentCapabilities_AgentCapabilities_AcceptsPackages) {
+			msgData.PackagesAvailable = msg.PackagesAvailable
+			msgData.PackageSyncer = NewPackagesSyncer(
+				r.logger,
+				msgData.PackagesAvailable,
+				r.sender,
+				r.clientSyncedState,
+				r.packagesStateProvider,
+			)
+		} else {
+			r.logger.Debugf("Ignoring PackagesAvailable, agent does not have AcceptsPackages capability")
+		}
+	}
+
+	if msg.AgentIdentification != nil {
+		err := r.rcvAgentIdentification(msg.AgentIdentification)
+		if err == nil {
+			msgData.AgentIdentification = msg.AgentIdentification
+		}
+	}
+
+	r.callbacks.OnMessage(ctx, msgData)
+	r.rcvOpampConnectionSettings(ctx, msg.ConnectionSettings)
+
+	if scheduled {
+		r.sender.ScheduleSend()
+	}
+
+	errResp := msg.GetErrorResponse()
+	if errResp != nil {
+		r.processErrorResponse(errResp)
 	}
 }
 
